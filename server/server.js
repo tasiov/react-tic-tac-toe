09db0c27f4c1b7deb91ec6442e0bb612c808fdad
Tasio Victoria <tasiovictoria@gmail.com>
2016-06-21 13:02:33 -0400

    changed port to work on heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ var routes = require('./config/routes.js');
 middleware(app, express);
 routes(app, express);
 
-app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Example app listening on port!');
 });
